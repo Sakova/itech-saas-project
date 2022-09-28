07abf11787f5f51432bfb7f041e930815d237853
Alexander Korchyk <aliaksandr.korchyk@gmail.com>
2022-09-28 18:12:20 +0300

    feat: config sending mail

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,13 @@ require_relative "application"
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+ActionMailer::Base.smtp_settings = {
+  port: 587,
+  address: 'smtp.gmail.com',
+  domain: 'heroku.com',
+  user_name: ENV['SMTP_USER_NAME'],
+  password: ENV['SMTP_PASSWORD'],
+  authentication: :plain,
+  enable_starttls_auto: true
+}
