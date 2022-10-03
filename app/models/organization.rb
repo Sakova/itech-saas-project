b4427f35ee1db694d6fe89bf11c79bec245b03f7
Alexander Korchyk <aliaksandr.korchyk@gmail.com>
2022-10-03 16:40:57 +0300

    feat: invate new user

--- app/models/organization.rb
+++ app/models/organization.rb
@@ -3,9 +3,10 @@ class Organization < ApplicationRecord
   has_many :projects, dependent: :destroy
   has_many :organization_users
   has_many :users, through: :organization_users
-  enum plan: %i[free premium]
 
-  validate :name, presence: true, unique: true
+  validates :name, uniqueness: true, presence: true
+
+  enum plan: %i[free premium]
 
   def self.create_new_organization(org_params)
 
